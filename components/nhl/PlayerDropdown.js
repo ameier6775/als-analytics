803d4300330ff449be16448b635bda13bd940ef3
Alex Meier <ameier6775@gmail.com>
2022-04-10 01:12:03 -0500

    Added fields to player card and changed defensive chart values

--- components/nhl/PlayerDropdown.js
+++ components/nhl/PlayerDropdown.js
@@ -25,15 +25,42 @@ const PlayerDropdown = ({ players }) => {
     setSelectedPlayer(e);
   };
 
-  // Preparing chart data
+  var allXgf = parseFloat(
+    selectedPlayer['I_F_flurryAdjustedxGoals'] +
+      selectedPlayer['I_F_flurryScoreVenueAdjustedxGoals'] +
+      selectedPlayer['I_F_highDangerxGoals'] +
+      selectedPlayer['I_F_lowDangerxGoals'] +
+      selectedPlayer['I_F_mediumDangerxGoals'] +
+      selectedPlayer['I_F_reboundxGoals'] +
+      selectedPlayer['I_F_scoreVenueAdjustedxGoals'] +
+      selectedPlayer['OnIce_F_reboundxGoals'] +
+      selectedPlayer['OnIce_F_scoreVenueAdjustedxGoals'] +
+      selectedPlayer['OnIce_F_xGoals'] +
+      selectedPlayer['OnIce_F_xGoalsFromActualReboundsOfShots'] +
+      selectedPlayer['OnIce_F_xGoalsFromxReboundsOfShots'] +
+      selectedPlayer['OnIce_F_xGoals_with_earned_rebounds'] +
+      selectedPlayer['OnIce_F_xGoals_with_earned_rebounds_scoreAdjusted'] +
+      selectedPlayer['OnIce_F_xGoals_with_earned_rebounds_scoreFlurryAdjusted'] +
+      selectedPlayer['OnIce_F_flurryAdjustedxGoals'] +
+      selectedPlayer['OnIce_F_flurryScoreVenueAdjustedxGoals'] +
+      selectedPlayer['OnIce_F_highDangerxGoals'] +
+      selectedPlayer['OnIce_F_lowDangerxGoals'] +
+      selectedPlayer['OnIce_F_mediumDangerxGoals'] +
+      selectedPlayer['I_F_xGoals'] +
+      selectedPlayer['I_F_xGoalsFromActualReboundsOfShots'] +
+      selectedPlayer['I_F_xGoalsFromxReboundsOfShots'] +
+      selectedPlayer['I_F_xGoals_with_earned_rebounds'] +
+      selectedPlayer['I_F_xGoals_with_earned_rebounds_scoreAdjusted'] +
+      selectedPlayer['I_F_xGoals_with_earned_rebounds_scoreFlurryAdjusted'] +
+      selectedPlayer['onIce_xGoalsPercentage'],
+  );
+  console.log(allXgf);
+
+  // Preparing offensive chart data
   let xGF =
     parseInt(getPercentile('OnIce_F_xGoals').substring(0, 2)) === 10
       ? 100
       : parseInt(getPercentile('OnIce_F_xGoals').substring(0, 2));
-  let xGA =
-    parseInt(getPercentile('OnIce_A_xGoals').substring(0, 2)) === 10
-      ? 100
-      : parseInt(getPercentile('OnIce_A_xGoals').substring(0, 2));
   let goals =
     parseInt(getPercentile('I_F_goals').substring(0, 2)) === 10
       ? 100
@@ -44,25 +71,32 @@ const PlayerDropdown = ({ players }) => {
     parseInt(getPercentile('I_F_points').substring(0, 2)) === 10
       ? 100
       : parseInt(getPercentile('I_F_points').substring(0, 2));
-  let gp =
-    parseInt(getPercentile('games_played').substring(0, 2)) === 10
+
+  // Preparing defensive chart data
+  let xGA =
+    parseInt(getPercentile('OnIce_A_xGoals').substring(0, 2)) === 10
       ? 100
-      : parseInt(getPercentile('games_played').substring(0, 2));
+      : parseInt(getPercentile('OnIce_A_xGoals').substring(0, 2));
+  let goalsAgainst =
+    parseInt(getPercentile('OnIce_A_goals').substring(0, 2)) === 10
+      ? 100
+      : !parseInt(getPercentile('OnIce_A_goals').substring(0, 2))
+      ? 0
+      : parseInt(getPercentile('OnIce_A_goals').substring(0, 2));
+  let giveaways =
+    parseInt(getPercentile('I_F_giveaways').substring(0, 2)) === 10
+      ? 100
+      : parseInt(getPercentile('I_F_giveaways').substring(0, 2));
 
   // Plotting chart data
-  var playerGraphData = [
+  var offensiveGraphData = [
     {
       category: 'xGF',
       score: xGF,
       fullMark: 100,
     },
     {
-      category: 'xGA',
-      score: xGA,
-      fullMark: 100,
-    },
-    {
-      category: 'Goals',
+      category: 'Goals For',
       score: goals,
       fullMark: 100,
     },
@@ -71,14 +105,26 @@ const PlayerDropdown = ({ players }) => {
       score: points,
       fullMark: 100,
     },
+  ];
+  var defensiveGraphData = [
     {
-      category: 'GP',
-      score: gp,
+      category: 'xGA',
+      score: xGA,
+      fullMark: 100,
+    },
+    {
+      category: 'Goals Against',
+      score: goalsAgainst,
+      fullMark: 100,
+    },
+    {
+      category: 'Giveaways',
+      score: giveaways,
       fullMark: 100,
     },
   ];
 
-  console.log(playerGraphData);
+  console.log(offensiveGraphData);
 
   // Gets the percentile for the given statistic
   function getPercentile(category) {
@@ -106,6 +152,34 @@ const PlayerDropdown = ({ players }) => {
       return (selectedPlayer[category] / maxValue).toFixed(2).substring(3, 4) + '%';
     } else {
       return (selectedPlayer[category] / maxValue).toFixed(2).substring(2, 4) + '%';
+    }
+  }
+  // Gets the percentile for the given statistic
+  function getNegativePercentile(category) {
+    // Group forwards & defensemen together
+    if (selectedPlayer['position'] === 'D') {
+      var positionalArr = players.filter((player) => player.position === 'D');
+    } else {
+      var positionalArr = players.filter((player) => player.position != 'D');
+    }
+
+    // Get the maximum player value for the statistic
+    var minValue = Math.min.apply(
+      Math,
+      positionalArr.map((obj) => {
+        return obj[category];
+      }),
+    );
+
+    // Specific conditions (100% & 0&)
+    if (minValue === selectedPlayer[category]) {
+      return '100%';
+    } else if (selectedPlayer[category] === 0) {
+      return 'N/A';
+    } else if ((selectedPlayer[category] / minValue).toFixed(2).substring(2, 3) === '0') {
+      return (selectedPlayer[category] / minValue).toFixed(2).substring(3, 4) + '%';
+    } else {
+      return (selectedPlayer[category] / minValue).toFixed(2).substring(2, 4) + '%';
     }
   }
 
@@ -151,26 +225,43 @@ const PlayerDropdown = ({ players }) => {
             Points: <span>{getPercentile('I_F_points')}</span>
           </p>
           <p>
-            TOI:{' '}
+            xGF: <span>{getPercentile('I_F_xGoals')}</span>
+          </p>
+          <p>
+            On Ice GF: <span>{getPercentile('OnIce_F_xGoals')}</span>
+          </p>
+          <p>
+            Hits: <span>{getPercentile('I_F_hits')}</span>
+          </p>
+          <p>
+            Blocks: <span>{getPercentile('shotsBlockedByPlayer')}</span>
+          </p>
+          <p>
+            Takeaways: <span>{getPercentile('I_F_takeaways')}</span>
+          </p>
+          <p>
+            xGA: <span>{getNegativePercentile('OnIce_A_xGoals')}</span>
+          </p>
+          <p>
+            On Ice GA: <span>{getPercentile('OnIce_A_goals')}</span>
+          </p>
+          <p>
+            Games: <span>{getPercentile('games_played')}</span>
+          </p>
+
+          <p>
+            Time On Ice:{' '}
             <span>
               {/* {(selectedPlayer.icetime / 60 / selectedPlayer.games_played).toFixed(2).substring(0, 4)} */}
               {getPercentile('icetime')}
             </span>
           </p>
           <p>
-            Games: <span>{getPercentile('games_played')}</span>
+            Team: <span>{selectedPlayer['team']}</span>
           </p>
+
           <p>
-            FO: <span>{getFaceOffPercentage()}</span>
-          </p>
-          <p>
-            xGoals: <span>{getPercentile('I_F_xGoals')}</span>
-          </p>
-          <p>
-            xGF: <span>{getPercentile('OnIce_F_xGoals')}</span>
-          </p>
-          <p>
-            xGA: <span>{getPercentile('OnIce_A_xGoals')}</span>
+            Faceoffs: <span>{getFaceOffPercentage()}</span>
           </p>
           <p>
             Game Score: <span>{getPercentile('gameScore')}</span>
@@ -178,9 +269,9 @@ const PlayerDropdown = ({ players }) => {
           <div className="fullWidth">
             <h2 className="playerChart">Offense</h2>
             <LineChart
-              width={625}
+              width={600}
               height={200}
-              data={playerGraphData}
+              data={offensiveGraphData}
               margin={{
                 top: 15,
                 right: 10,
@@ -218,9 +309,9 @@ const PlayerDropdown = ({ players }) => {
           <div className="fullWidth">
             <h2 className="playerChart">Defense</h2>
             <LineChart
-              width={625}
+              width={600}
               height={200}
-              data={playerGraphData}
+              data={defensiveGraphData}
               margin={{
                 top: 15,
                 right: 10,
@@ -233,8 +324,8 @@ const PlayerDropdown = ({ players }) => {
               <YAxis stroke="white" />
               <ReferenceLine
                 y={100}
-                label={{ value: 'Best', angle: 0, position: 'top', fill: 'lightgreen', fontSize: 12 }}
-                stroke="lightgreen"
+                label={{ value: 'Worst', angle: 0, position: 'top', fill: 'red', fontSize: 12 }}
+                stroke="red"
               />
               <ReferenceLine
                 y={50}
@@ -243,8 +334,8 @@ const PlayerDropdown = ({ players }) => {
               />
               <ReferenceLine
                 y={0}
-                label={{ value: 'Worst', angle: 0, position: 'top', fill: 'red', fontSize: 12 }}
-                stroke="red"
+                label={{ value: 'Best', angle: 0, position: 'top', fill: 'lightgreen', fontSize: 12 }}
+                stroke="lightgreen"
               />
               <Line
                 type="monotone"
@@ -264,3 +355,171 @@ const PlayerDropdown = ({ players }) => {
 };
 
 export default PlayerDropdown;
+
+{
+  /* Offensive */
+}
+// I_F_flurryAdjustedxGoals
+// I_F_flurryScoreVenueAdjustedxGoals
+// I_F_goals
+// I_F_highDangerGoals
+// I_F_highDangerShots
+// I_F_highDangerxGoals
+// I_F_lowDangerGoals
+// I_F_lowDangerShots
+// I_F_lowDangerxGoals
+// I_F_mediumDangerGoals
+// I_F_mediumDangerShots
+// I_F_mediumDangerxGoals
+// I_F_oZoneShiftEnds
+// I_F_oZoneShiftStarts
+// I_F_points
+// I_F_primaryAssists
+// I_F_reboundGoals
+// I_F_rebounds
+// I_F_reboundxGoals
+// I_F_savedShotsOnGoal
+// I_F_savedUnblockedShotAttempts
+// I_F_scoreAdjustedShotsAttempts
+// I_F_scoreAdjustedUnblockedShotAttempts
+// I_F_scoreVenueAdjustedxGoals
+// I_F_secondaryAssists
+// I_F_playContinuedInZone
+// I_F_shotsOnGoal
+// I_F_shotAttempts
+// OnIce_F_reboundGoals
+// OnIce_F_rebounds
+// OnIce_F_reboundxGoals
+// OnIce_F_scoreAdjustedShotsAttempts
+// OnIce_F_scoreAdjustedUnblockedShotAttempts
+// OnIce_F_scoreVenueAdjustedxGoals
+// OnIce_F_shotAttempts
+// OnIce_F_shotsOnGoal
+// OnIce_F_unblockedShotAttempts
+// OnIce_F_xGoals
+// OnIce_F_xGoalsFromActualReboundsOfShots
+// OnIce_F_xGoalsFromxReboundsOfShots
+// OnIce_F_xGoals_with_earned_rebounds
+// OnIce_F_xGoals_with_earned_rebounds_scoreAdjusted
+// OnIce_F_xGoals_with_earned_rebounds_scoreFlurryAdjusted
+// OnIce_F_xOnGoal
+// I_F_xRebounds
+// OnIce_F_blockedShotAttempts
+// OnIce_F_flurryAdjustedxGoals
+// OnIce_F_flurryScoreVenueAdjustedxGoals
+// OnIce_F_goals
+// OnIce_F_highDangerGoals
+// OnIce_F_highDangerShots
+// OnIce_F_highDangerxGoals
+// OnIce_F_lowDangerGoals
+// OnIce_F_lowDangerShots
+// OnIce_F_lowDangerxGoals
+// OnIce_F_mediumDangerGoals
+// OnIce_F_mediumDangerShots
+// OnIce_F_mediumDangerxGoals
+// I_F_xGoals
+// I_F_xGoalsFromActualReboundsOfShots
+// I_F_xGoalsFromxReboundsOfShots
+// I_F_xGoals_with_earned_rebounds
+// I_F_xGoals_with_earned_rebounds_scoreAdjusted
+// I_F_xGoals_with_earned_rebounds_scoreFlurryAdjusted
+// I_F_xOnGoal
+// I_F_unblockedShotAttempts
+// I_F_xPlayContinuedInZone
+// onIce_corsiPercentage
+// onIce_fenwickPercentage
+// onIce_xGoalsPercentage
+// fenwickForAfterShifts
+
+{
+  /* Defensive */
+}
+// I_F_blockedShotAttempts
+// I_F_dZoneGiveaways
+// I_F_dZoneShiftEnds
+// I_F_dZoneShiftStarts
+// I_F_giveaways
+// I_F_hits
+// I_F_takeaways
+// OnIce_A_blockedShotAttempts
+// OnIce_A_flurryAdjustedxGoals
+// OnIce_A_flurryScoreVenueAdjustedxGoals
+// OnIce_A_goals
+// OnIce_A_highDangerGoals
+// OnIce_A_highDangerShots
+// OnIce_A_highDangerxGoals
+// OnIce_A_lowDangerGoals
+// OnIce_A_lowDangerShots
+// OnIce_A_lowDangerxGoals
+// OnIce_A_mediumDangerGoals
+// OnIce_A_mediumDangerShots
+// OnIce_A_mediumDangerxGoals
+// OnIce_A_missedShots
+// OnIce_A_reboundGoals
+// OnIce_A_rebounds
+// OnIce_A_reboundxGoals
+// OnIce_A_scoreAdjustedShotsAttempts
+// OnIce_A_scoreAdjustedUnblockedShotAttempts
+// OnIce_A_scoreVenueAdjustedxGoals
+// OnIce_A_shotAttempts
+// OnIce_A_shotsOnGoal
+// OnIce_A_unblockedShotAttempts
+// OnIce_A_xGoals
+// OnIce_A_xGoalsFromActualReboundsOfShots
+// OnIce_A_xGoalsFromxReboundsOfShots
+// OnIce_A_xGoals_with_earned_rebounds
+// OnIce_A_xGoals_with_earned_rebounds_scoreAdjusted
+// OnIce_A_xGoals_with_earned_rebounds_scoreFlurryAdjusted
+// OnIce_A_xOnGoal
+// shotsBlockedByPlayer
+// fenwickAgainstAfterShifts
+// xGoalsAgainstAfterShifts
+
+{
+  /* Neutral */
+}
+// I_F_faceOffsWon
+// I_F_flyShiftEnds
+// I_F_flyShiftStarts
+// I_F_missedShots
+// I_F_neutralZoneShiftEnds
+// I_F_neutralZoneShiftStarts
+// I_F_penalityMinutes
+// I_F_shifts
+// OffIce_A_shotAttempts
+// OffIce_A_xGoals
+// OffIce_F_shotAttempts
+// OffIce_F_xGoals
+// xGoalsForAfterShifts
+// gameScore
+// games_played
+// iceTimeRank
+// icetime
+// corsiForAfterShifts
+// corsiAgainstAfterShifts
+// offIce_corsiPercentage
+// offIce_fenwickPercentage
+// offIce_xGoalsPercentage
+// shifts
+// situation
+// team
+// timeOnBench
+// faceoffsLost
+// faceoffsWon
+// penalityMinutes
+// penalityMinutesDrawn
+// penalties
+// penaltiesDrawn
+// expectedGoalDifferential
+
+{
+  /* Need some clarity */
+}
+// I_F_playStopped
+// I_F_freeze
+// I_F_playContinuedOutsideZone
+// I_F_xFreeze
+// I_F_xPlayContinuedOutsideZone
+// I_F_xPlayStopped
+// OnIce_F_missedShots
+// actualOnIceGoalsAgainst

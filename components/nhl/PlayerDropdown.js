d0441693df376a9131b6b6fd53ad2bf33ea1e9d8
Alex Meier <ameier6775@gmail.com>
2022-04-13 23:57:04 -0500

    Rearranging and grouping some of the data fields

--- components/nhl/PlayerDropdown.js
+++ components/nhl/PlayerDropdown.js
@@ -65,7 +65,7 @@ const PlayerDropdown = ({ players }) => {
       fullMark: 100,
     },
     {
-      category: 'Goals For',
+      category: '🧊 GF Per 60',
       score: goals,
       fullMark: 100,
     },
@@ -82,7 +82,7 @@ const PlayerDropdown = ({ players }) => {
       fullMark: 100,
     },
     {
-      category: 'Goals Against',
+      category: '🧊 GA Per 60',
       score: goalsAgainst,
       fullMark: 100,
     },
@@ -384,177 +384,195 @@ export default PlayerDropdown;
 {
   /* Offensive */
 }
-// Pure Production Sub Category
-// I_F_goals
-// I_F_highDangerGoals
-// I_F_highDangerShots
+// Goals For
 // I_F_lowDangerGoals
-// I_F_lowDangerShots
+// I_F_lowDangerxGoals
 // I_F_mediumDangerGoals
-// I_F_mediumDangerShots
-// I_F_points
-// I_F_primaryAssists
+// I_F_mediumDangerxGoals
+// I_F_highDangerGoals
+// I_F_highDangerxGoals
+// OnIce_F_lowDangerGoals
+// OnIce_F_lowDangerxGoals
+// OnIce_F_mediumDangerGoals
+// OnIce_F_mediumDangerxGoals
+// OnIce_F_highDangerGoals
+// OnIce_F_highDangerxGoals
+
+// Net Front
+// OnIce_F_rebounds
+// OnIce_F_reboundGoals
 // I_F_reboundGoals
 // I_F_rebounds
-// I_F_savedShotsOnGoal
-// I_F_savedUnblockedShotAttempts
-// I_F_secondaryAssists
-// I_F_shotsOnGoal
-// I_F_shotAttempts
-// I_F_unblockedShotAttempts
-// OnIce_F_reboundGoals
-// OnIce_F_rebounds
-// OnIce_F_shotAttempts
-// OnIce_F_shotsOnGoal
-// OnIce_F_unblockedShotAttempts
-// OnIce_F_blockedShotAttempts
-// OnIce_F_highDangerGoals
-// OnIce_F_highDangerShots
-// OnIce_F_goals
-// OnIce_F_lowDangerGoals
-// OnIce_F_lowDangerShots
-// OnIce_F_mediumDangerGoals
-// OnIce_F_mediumDangerShots
-// onIce_corsiPercentage
-// onIce_fenwickPercentage
-
-// Anything Expected / Adjusted
 // I_F_flurryAdjustedxGoals
 // I_F_flurryScoreVenueAdjustedxGoals
-// I_F_highDangerxGoals
-// I_F_lowDangerxGoals
-// I_F_mediumDangerxGoals
 // I_F_reboundxGoals
-// I_F_scoreAdjustedShotsAttempts
-// I_F_scoreAdjustedUnblockedShotAttempts
-// I_F_scoreVenueAdjustedxGoals
 // I_F_xRebounds
-// I_F_xGoals
 // I_F_xGoalsFromActualReboundsOfShots
 // I_F_xGoalsFromxReboundsOfShots
 // I_F_xGoals_with_earned_rebounds
-// I_F_xGoals_with_earned_rebounds_scoreAdjusted
 // I_F_xGoals_with_earned_rebounds_scoreFlurryAdjusted
-// I_F_xOnGoal
-// I_F_xPlayContinuedInZone
-// OnIce_F_scoreAdjustedShotsAttempts
-// OnIce_F_scoreAdjustedUnblockedShotAttempts
-// OnIce_F_scoreVenueAdjustedxGoals
 // OnIce_F_reboundxGoals
-// OnIce_F_xGoals
 // OnIce_F_xGoalsFromActualReboundsOfShots
 // OnIce_F_xGoalsFromxReboundsOfShots
 // OnIce_F_xGoals_with_earned_rebounds
 // OnIce_F_xGoals_with_earned_rebounds_scoreAdjusted
 // OnIce_F_xGoals_with_earned_rebounds_scoreFlurryAdjusted
-// OnIce_F_xOnGoal
 // OnIce_F_flurryAdjustedxGoals
-// OnIce_F_flurryScoreVenueAdjustedxGoals
-// OnIce_F_highDangerxGoals
-// OnIce_F_lowDangerxGoals
-// OnIce_F_mediumDangerxGoals
+
+// Play Making
+// I_F_unblockedShotAttempts
+// I_F_savedUnblockedShotAttempts
+// I_F_shotAttempts
+// OnIce_F_shotAttempts
+// OnIce_F_unblockedShotAttempts
+// OnIce_F_blockedShotAttempts
+// onIce_corsiPercentage
+// onIce_fenwickPercentage
+
+// Scoring
+// I_F_primaryAssists
+// I_F_secondaryAssists
+// I_F_goals
+// I_F_xGoals
+// I_F_points
+// I_F_xOnGoal
+// OnIce_F_goals
+// OnIce_F_xGoals
+// OnIce_F_xOnGoal
 // onIce_xGoalsPercentage
 
-// Zones / Shifts
-// I_F_oZoneShiftEnds
-// I_F_oZoneShiftStarts
-// I_F_playContinuedInZone
-// fenwickForAfterShifts
+// Shots
+// I_F_lowDangerShots
+// I_F_mediumDangerShots
+// I_F_highDangerShots
+// I_F_shotsOnGoal
+// I_F_savedShotsOnGoal
+// OnIce_F_lowDangerShots
+// OnIce_F_mediumDangerShots
+// OnIce_F_highDangerShots
+// OnIce_F_shotsOnGoal
 
+// Situational Play
+// I_F_scoreVenueAdjustedxGoals
+// I_F_scoreAdjustedShotsAttempts
+// I_F_scoreAdjustedUnblockedShotAttempts
+// I_F_xGoals_with_earned_rebounds_scoreAdjusted
+// OnIce_F_scoreVenueAdjustedxGoals
+// OnIce_F_scoreAdjustedShotsAttempts
+// OnIce_F_scoreAdjustedUnblockedShotAttempts
+// OnIce_F_flurryScoreVenueAdjustedxGoals
 {
   /* Defensive */
 }
-// Pure Production Sub Category
-// I_F_blockedShotAttempts
-// I_F_dZoneGiveaways
-// I_F_giveaways
-// I_F_hits
-// I_F_takeaways
-// OnIce_A_blockedShotAttempts
+// Goals Against
 // OnIce_A_goals
-// OnIce_A_highDangerGoals
-// OnIce_A_highDangerShots
-// OnIce_A_lowDangerGoals
-// OnIce_A_lowDangerShots
-// OnIce_A_mediumDangerGoals
-// OnIce_A_mediumDangerShots
-// OnIce_A_missedShots
-// OnIce_A_reboundGoals
-// OnIce_A_rebounds
-// OnIce_A_shotAttempts
-// OnIce_A_shotsOnGoal
-// OnIce_A_unblockedShotAttempts
-// shotsBlockedByPlayer
-
-// Anything Expected / Adjusted
-// OnIce_A_flurryAdjustedxGoals
-// OnIce_A_flurryScoreVenueAdjustedxGoals
-// OnIce_A_highDangerxGoals
-// OnIce_A_lowDangerxGoals
-// OnIce_A_mediumDangerxGoals
-// OnIce_A_reboundxGoals
-// OnIce_A_scoreAdjustedShotsAttempts
-// OnIce_A_scoreAdjustedUnblockedShotAttempts
-// OnIce_A_scoreVenueAdjustedxGoals
 // OnIce_A_xGoals
+// OnIce_A_xOnGoal
+// OnIce_A_lowDangerGoals
+// OnIce_A_lowDangerxGoals
+// OnIce_A_mediumDangerGoals
+// OnIce_A_mediumDangerxGoals
+// OnIce_A_highDangerGoals
+// OnIce_A_highDangerxGoals
+
+// Physicality
+// shotsBlockedByPlayer
+// I_F_blockedShotAttempts
+// I_F_hits
+// OnIce_A_blockedShotAttempts
+// OnIce_A_reboundGoals
+// OnIce_A_reboundxGoals
 // OnIce_A_xGoalsFromActualReboundsOfShots
 // OnIce_A_xGoalsFromxReboundsOfShots
 // OnIce_A_xGoals_with_earned_rebounds
+
+// Puck Management
+// I_F_dZoneGiveaways
+// I_F_giveaways
+// I_F_takeaways
+
+// Shots Against
+// OnIce_A_lowDangerShots
+// OnIce_A_mediumDangerShots
+// OnIce_A_highDangerShots
+// OnIce_A_shotsOnGoal
+// OnIce_A_missedShots
+// OnIce_A_shotAttempts
+// OnIce_A_unblockedShotAttempts
+// OnIce_A_rebounds
+
+// Situational
+// OnIce_A_flurryAdjustedxGoals
+// OnIce_A_flurryScoreVenueAdjustedxGoals
+// OnIce_A_scoreAdjustedShotsAttempts
+// OnIce_A_scoreAdjustedUnblockedShotAttempts
+// OnIce_A_scoreVenueAdjustedxGoals
 // OnIce_A_xGoals_with_earned_rebounds_scoreAdjusted
 // OnIce_A_xGoals_with_earned_rebounds_scoreFlurryAdjusted
-// OnIce_A_xOnGoal
-// xGoalsAgainstAfterShifts
-
-// Zones / Shifts
-// I_F_dZoneShiftEnds
-// I_F_dZoneShiftStarts
-// fenwickAgainstAfterShifts
 {
   /* Neutral */
 }
-// Pure Production Sub Category
-// I_F_faceOffsWon
-// I_F_flyShiftEnds
-// I_F_missedShots
-// I_F_penalityMinutes
-// I_F_shifts
-// OffIce_A_shotAttempts
-// OffIce_F_shotAttempts
-// games_played
-// iceTimeRank
-// icetime
+// Gaps
+// I_F_playContinuedInZone
+// I_F_xPlayContinuedInZone
+// I_F_playContinuedOutsideZone
+// I_F_xPlayContinuedOutsideZone
+
+// Line Changes
+// xGoalsForAfterShifts
+// xGoalsAgainstAfterShifts
+// fenwickForAfterShifts
+// fenwickAgainstAfterShifts
 // corsiForAfterShifts
 // corsiAgainstAfterShifts
-// offIce_corsiPercentage
-// offIce_fenwickPercentage
-// situation
-// team
-// timeOnBench
-// faceoffsLost
-// faceoffsWon
+
+// Penalties
+// I_F_penalityMinutes
 // penalityMinutes
 // penalityMinutesDrawn
 // penalties
 // penaltiesDrawn
-// I_F_playStopped
-// I_F_freeze
-// I_F_playContinuedOutsideZone
-// OnIce_F_missedShots
-// actualOnIceGoalsAgainst
 
-// Anything Expected / Adjusted
+// Production
+// gameScore
+// I_F_faceOffsWon
+// faceoffsLost
+// faceoffsWon
+// expectedGoalDifferential
+// I_F_missedShots
+
+// Other
+// games_played
+// situation
+// team
+// OnIce_F_missedShots
+
+// Shifts
+// icetime
+// iceTimeRank
+// timeOnBench
+// shifts
+// I_F_shifts
+// I_F_dZoneShiftStarts
+// I_F_dZoneShiftEnds
+// I_F_flyShiftStarts
+// I_F_flyShiftEnds
+// I_F_neutralZoneShiftStarts
+// I_F_neutralZoneShiftEnds
+// I_F_oZoneShiftEnds
+// I_F_oZoneShiftStarts
+
+// Value
+// OffIce_A_shotAttempts
+// OffIce_F_shotAttempts
+// offIce_corsiPercentage
+// offIce_fenwickPercentage
 // OffIce_A_xGoals
 // OffIce_F_xGoals
-// gameScore
-// xGoalsForAfterShifts
 // offIce_xGoalsPercentage
-// expectedGoalDifferential
-// I_F_xFreeze
-// I_F_xPlayContinuedOutsideZone
-// I_F_xPlayStopped
 
-// Zones / Shifts
-// I_F_flyShiftStarts
-// I_F_neutralZoneShiftEnds
-// I_F_neutralZoneShiftStarts
-// shifts
+// Zones
+// I_F_playStopped
+// I_F_xPlayStopped
+// I_F_freeze
+// I_F_xFreeze

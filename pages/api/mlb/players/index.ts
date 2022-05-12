50ebceb599bbb58182d08c98280bb10aa4f4c32a
Alex Meier <ameier6775@gmail.com>
2022-05-11 23:16:46 -0500

    Filtering the hitters by the number of at bats

--- pages/api/mlb/players/index.ts
+++ pages/api/mlb/players/index.ts
@@ -6,22 +6,22 @@ export default async function handler(req, res) {
   const dir = path.resolve('../stats-app/data/mlb/players/2022');
 
   // Hitters Standard Data
-  const columnArrayHitterFGStandard =
+  const columnArrayHitterStandard =
     'Player Team Pos Age G AB R H 2B 3B HR RBI SB CS BB SO SH SF HBP AVG OBP SLG OPS'.split(/\s+/);
 
-  type ColumnHeaderForHitterFGStandard = { [C in typeof columnArrayHitterFGStandard[number]]: number };
-  const columnHeaderMapHitterFGStandard: ColumnHeaderForHitterFGStandard = columnArrayHitterFGStandard.reduce(
+  type ColumnHeaderForHitterStandard = { [C in typeof columnArrayHitterStandard[number]]: number };
+  const columnHeaderMapHitterStandard: ColumnHeaderForHitterStandard = columnArrayHitterStandard.reduce(
     (acc, value, index) => ({ ...acc, [value]: index + 1 }),
     {},
   );
 
-  const workbookHitterFGStandard = new Excel.Workbook();
-  await workbookHitterFGStandard.csv.readFile(`${dir}/Hitters.csv`);
-  const worksheetHitterFGStandard = workbookHitterFGStandard.worksheets[0];
-  const dataHitterFGStandard = [];
+  const workbookHitterStandard = new Excel.Workbook();
+  await workbookHitterStandard.csv.readFile(`${dir}/Hitters.csv`);
+  const worksheetHitterStandard = workbookHitterStandard.worksheets[0];
+  const dataHitterStandard = [];
 
-  worksheetHitterFGStandard.eachRow((row, rowNumber) => {
-    const rowObject = Object.entries(columnHeaderMapHitterFGStandard).reduce((acc, value) => {
+  worksheetHitterStandard.eachRow((row, rowNumber) => {
+    const rowObject = Object.entries(columnHeaderMapHitterStandard).reduce((acc, value) => {
       return {
         ...acc,
         [value[0]]: row.values[value[1]],
@@ -29,40 +29,31 @@ export default async function handler(req, res) {
     }, {});
 
     if (rowNumber !== 1) {
-      dataHitterFGStandard.push(rowObject);
+      dataHitterStandard.push(rowObject);
     }
   });
 
+  // Filter out hitters with very few at bats
+  const hittersArray = dataHitterStandard.filter((player) => parseInt(player.AB) > 50);
+
   //   Joining the two hitter fan graphs tables by team name
   const data = [];
-  for (let i = 0; i < dataHitterFGStandard.length; i++) {
-    const standardHitterElement = dataHitterFGStandard[i];
+  for (let i = 0; i < hittersArray.length; i++) {
+    const standardHitterElement = hittersArray[i];
 
     // Adding these two fields for the dropdown
     standardHitterElement.value = standardHitterElement['Player'];
     standardHitterElement.label = standardHitterElement['Player'];
 
-    // Finding advanced data object by the standards team name
-    //   const advancedHitterElement = dataHitterFGAdvanced.find((obj) => obj['Player'] === standardHitterElement['Player']);
-    //   const standardPitcherElement = dataPitcherFGStandard.find(
-    //     (obj) => obj['Player'] === standardHitterElement['Player'],
-    //   );
-    //   const advancedPitcherElement = dataPitcherFGAdvanced.find(
-    //     (obj) => obj['Player'] === standardHitterElement['Player'],
-    //   );
-
     // Putting both objects values into one & inserting it into the array
     const element = {
       ...standardHitterElement,
-      //     ...advancedHitterElement,
-      //     ...standardPitcherElement,
-      //     ...advancedPitcherElement,
     };
     data.push(element);
   }
 
   // Response
   res.status(200).json({
-    data: dataHitterFGStandard,
+    data: data,
   });
 }

0a1dea934fb00d72068d5ae079ecfe43d48f2d97
Alex Meier <ameier6775@gmail.com>
2022-05-14 16:02:28 -0500

    Adding PitcherCard component to the page

--- pages/mlb/players/index.js
+++ pages/mlb/players/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Link from 'next/link';
 import useSWR from 'swr';
 import HitterCard from '../../../components/mlb/HitterCard';
+import PitcherCard from '../../../components/mlb/PitcherCard';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -10,11 +11,12 @@ export default function Teams() {
   const { data, error } = useSWR('../api/mlb/players/', fetcher);
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
-  console.log(data.data);
+  console.log(data);
 
   return (
     <>
-      <HitterCard players={data.data}></HitterCard>
+      <HitterCard players={data.hittersData}></HitterCard>
+      <PitcherCard players={data.pitchersData}></PitcherCard>
     </>
   );
 }

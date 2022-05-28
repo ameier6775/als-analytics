943a1cf0e26d3a268bc11c4132a53b3b406c88ec
Alex Meier <ameier6775@gmail.com>
2022-05-28 16:21:18 -0500

    Switching from CSV files to MySQL db

--- pages/api/nhl/teams/index.ts
+++ pages/api/nhl/teams/index.ts
@@ -1,155 +1,25 @@
 import Excel from 'exceljs';
-import _, { groupBy, keyBy, omit, pick, orderBy } from 'lodash';
+import _, { groupBy, keyBy, omit, pick, orderBy, result } from 'lodash';
 import path from 'path';
+import excuteQuery from '../../../../lib/db';
 
 export default async function handler(req, res) {
-  const dir = path.resolve('../stats-app/data/nhl/teams/2022');
-  // Hockey-Reference Stats Data
-  const columnArrayStats =
-    'teamId name averageAge gamesPlayed wins losses otLosses points pointsPercentage overallGoalsFor overallGoalsAgainst soWins soLosses ratingSystem strengthOfSchedule goalsForPerGame goalsAgainstPerGame ppGoalsFor ppChancesFor ppPercentage ppGoalsAgainst ppChancesAgainst pkPercentage shGoalsFor shGoalsAgainst pimPerG pimDrawnPerGame saves savePercentage shotsAgainst svPercentage shutouts'.split(
-      /\s+/,
-    );
-  type ColumnHeaderForTeamsStats = { [C in typeof columnArrayStats[number]]: number };
-  const columnHeaderMapStats: ColumnHeaderForTeamsStats = columnArrayStats.reduce(
-    (acc, value, index) => ({ ...acc, [value]: index + 1 }),
-    {},
-  );
-  const workbookStats = new Excel.Workbook();
-  await workbookStats.csv.readFile(`${dir}/stats.csv`);
-  const worksheetStats = workbookStats.worksheets[0];
-  const dataStats = [];
-  worksheetStats.eachRow((row, rowNumber) => {
-    const rowObject = Object.entries(columnHeaderMapStats).reduce((acc, value) => {
-      return {
-        ...acc,
-        [value[0]]: row.values[value[1]],
-      };
-    }, {});
-    if (rowNumber !== 1) {
-      dataStats.push(rowObject);
-    }
-  });
+  if (req.method === 'POST') {
+    const result = await excuteQuery({
+      query:
+        "SELECT team, goalsFor, RANK() OVER (ORDER BY goalsFor DESC) goalsForRank, ROUND((goalsFor - (SELECT MIN(goalsFor) FROM NHL_2021.teams_moneypuck WHERE situation = 'all')) / (SELECT MAX(goalsFor) - MIN(goalsFor) FROM NHL_2021.teams_moneypuck WHERE situation = 'all'), 2) AS goalsForRate, xGoalsFor, RANK() OVER (ORDER BY xGoalsFor DESC) xGoalsForRank, ROUND((xGoalsFor - (SELECT MIN(xGoalsFor) FROM NHL_2021.teams_moneypuck WHERE situation = 'all')) / (SELECT MAX(xGoalsFor) - MIN(xGoalsFor) FROM NHL_2021.teams_moneypuck WHERE situation = 'all'), 2) AS xGoalsForRate, takeawaysFor, RANK() OVER (ORDER BY takeawaysFor DESC) takeawaysForRank, ROUND((takeawaysFor - (SELECT MIN(takeawaysFor) FROM NHL_2021.teams_moneypuck WHERE situation = 'all')) / (SELECT MAX(takeawaysFor) - MIN(takeawaysFor) FROM NHL_2021.teams_moneypuck WHERE situation = 'all'), 2) AS takeawaysForRate,  shotAttemptsFor, RANK() OVER (ORDER BY shotAttemptsFor DESC) shotAttemptsForRank, ROUND((shotAttemptsFor - (SELECT MIN(shotAttemptsFor) FROM NHL_2021.teams_moneypuck WHERE situation = 'all')) / (SELECT MAX(shotAttemptsFor) - MIN(shotAttemptsFor) FROM NHL_2021.teams_moneypuck WHERE situation = 'all'), 2) AS shotAttemptsForRate, goalsAgainst, RANK() OVER (ORDER BY goalsAgainst) goalsAgainstRank, ROUND(1 - (goalsAgainst - (SELECT MIN(goalsAgainst) FROM NHL_2021.teams_moneypuck WHERE situation = 'all')) / (SELECT MAX(goalsAgainst) - MIN(goalsAgainst) FROM NHL_2021.teams_moneypuck WHERE situation = 'all'), 2) AS goalsAgainstRate, xGoalsAgainst, RANK() OVER (ORDER BY xGoalsAgainst) xGoalsAgainstRank, ROUND(1 - (xGoalsAgainst - (SELECT MIN(xGoalsAgainst) FROM NHL_2021.teams_moneypuck WHERE situation = 'all')) / (SELECT MAX(xGoalsAgainst) - MIN(xGoalsAgainst) FROM NHL_2021.teams_moneypuck WHERE situation = 'all'), 2) AS xGoalsAgainstRate, giveawaysFor, RANK() OVER (ORDER BY giveawaysFor) giveawaysForRank, ROUND(1 - (giveawaysFor - (SELECT MIN(giveawaysFor) FROM NHL_2021.teams_moneypuck WHERE situation = 'all')) / (SELECT MAX(giveawaysFor) - MIN(giveawaysFor) FROM NHL_2021.teams_moneypuck WHERE situation = 'all'), 2) AS giveawaysForRate, shotAttemptsAgainst, RANK() OVER (ORDER BY shotAttemptsAgainst) shotAttemptsAgainstRank, ROUND(1 - (shotAttemptsAgainst - (SELECT MIN(shotAttemptsAgainst) FROM NHL_2021.teams_moneypuck WHERE situation = 'all')) / (SELECT MAX(shotAttemptsAgainst) - MIN(shotAttemptsAgainst) FROM NHL_2021.teams_moneypuck WHERE situation = 'all'), 2) AS shotAttemptsAgainstRate, ROUND(faceOffsWonFor / (faceOffsWonFor + faceOffsWonAgainst), 2) AS faceoffPercentage, RANK() OVER (ORDER BY faceOffsWonFor / (faceOffsWonFor + faceOffsWonAgainst) DESC) faceoffsRank, penaltiesAgainst - penaltiesFor AS penaltyDifferential, RANK() OVER (ORDER BY penaltiesFor - penaltiesAgainst) AS penaltyRank FROM NHL_2021.teams_moneypuck AS rankings WHERE situation = 'all' ORDER BY team;",
+      values: [req.body.content],
+    });
+    res.json(result);
+    console.log('posting data ' + req.body.team);
+  } else if (req.method === 'GET') {
+    const result = await excuteQuery({
+      query:
+        "SELECT team AS value, team AS label, team, goalsFor, RANK() OVER (ORDER BY goalsFor DESC) goalsForRank, ROUND((goalsFor - (SELECT MIN(goalsFor) FROM NHL_2021.teams_moneypuck WHERE situation = 'all')) / (SELECT MAX(goalsFor) - MIN(goalsFor) FROM NHL_2021.teams_moneypuck WHERE situation = 'all'), 2) AS goalsForRate, xGoalsFor, RANK() OVER (ORDER BY xGoalsFor DESC) xGoalsForRank, ROUND((xGoalsFor - (SELECT MIN(xGoalsFor) FROM NHL_2021.teams_moneypuck WHERE situation = 'all')) / (SELECT MAX(xGoalsFor) - MIN(xGoalsFor) FROM NHL_2021.teams_moneypuck WHERE situation = 'all'), 2) AS xGoalsForRate, takeawaysFor, RANK() OVER (ORDER BY takeawaysFor DESC) takeawaysForRank, ROUND((takeawaysFor - (SELECT MIN(takeawaysFor) FROM NHL_2021.teams_moneypuck WHERE situation = 'all')) / (SELECT MAX(takeawaysFor) - MIN(takeawaysFor) FROM NHL_2021.teams_moneypuck WHERE situation = 'all'), 2) AS takeawaysForRate,  shotAttemptsFor, RANK() OVER (ORDER BY shotAttemptsFor DESC) shotAttemptsForRank, ROUND((shotAttemptsFor - (SELECT MIN(shotAttemptsFor) FROM NHL_2021.teams_moneypuck WHERE situation = 'all')) / (SELECT MAX(shotAttemptsFor) - MIN(shotAttemptsFor) FROM NHL_2021.teams_moneypuck WHERE situation = 'all'), 2) AS shotAttemptsForRate, goalsAgainst, RANK() OVER (ORDER BY goalsAgainst) goalsAgainstRank, ROUND(1 - (goalsAgainst - (SELECT MIN(goalsAgainst) FROM NHL_2021.teams_moneypuck WHERE situation = 'all')) / (SELECT MAX(goalsAgainst) - MIN(goalsAgainst) FROM NHL_2021.teams_moneypuck WHERE situation = 'all'), 2) AS goalsAgainstRate, xGoalsAgainst, RANK() OVER (ORDER BY xGoalsAgainst) xGoalsAgainstRank, ROUND(1 - (xGoalsAgainst - (SELECT MIN(xGoalsAgainst) FROM NHL_2021.teams_moneypuck WHERE situation = 'all')) / (SELECT MAX(xGoalsAgainst) - MIN(xGoalsAgainst) FROM NHL_2021.teams_moneypuck WHERE situation = 'all'), 2) AS xGoalsAgainstRate, giveawaysFor, RANK() OVER (ORDER BY giveawaysFor) giveawaysForRank, ROUND(1 - (giveawaysFor - (SELECT MIN(giveawaysFor) FROM NHL_2021.teams_moneypuck WHERE situation = 'all')) / (SELECT MAX(giveawaysFor) - MIN(giveawaysFor) FROM NHL_2021.teams_moneypuck WHERE situation = 'all'), 2) AS giveawaysForRate, shotAttemptsAgainst, RANK() OVER (ORDER BY shotAttemptsAgainst) shotAttemptsAgainstRank, ROUND(1 - (shotAttemptsAgainst - (SELECT MIN(shotAttemptsAgainst) FROM NHL_2021.teams_moneypuck WHERE situation = 'all')) / (SELECT MAX(shotAttemptsAgainst) - MIN(shotAttemptsAgainst) FROM NHL_2021.teams_moneypuck WHERE situation = 'all'), 2) AS shotAttemptsAgainstRate, ROUND(faceOffsWonFor / (faceOffsWonFor + faceOffsWonAgainst), 2) AS faceoffPercentage, RANK() OVER (ORDER BY faceOffsWonFor / (faceOffsWonFor + faceOffsWonAgainst) DESC) faceoffsRank, penaltiesAgainst - penaltiesFor AS penaltyDifferential, RANK() OVER (ORDER BY penaltiesFor - penaltiesAgainst) AS penaltyRank FROM NHL_2021.teams_moneypuck AS rankings WHERE situation = 'all' ORDER BY team;",
 
-  // Hockey-Reference Analytics Data
-  const columnArrayAnalytics =
-    'teamId name shPercentage svPercentage pdo corsiFor corsiAgainst corsiForPercentage fenwickFor fenwickAgainst fenwickForPercentage xGoalsFor xGoalsAgainst actualGoalsFor actualGoalsAgainst actualGoalDifferential scoringChancesFor scoringChancesAgainst scoringChancesForPercentage highDangerChancesFor highDangerChancesAgainst highDangerChancesForPercentage highDangerGoalsFor highDangerConversionRateFor highDangerGoalsAgainst highDangerConversionRateAgainst'.split(
-      /\s+/,
-    );
-
-  type ColumnHeaderForTeamsAnalytics = { [C in typeof columnArrayAnalytics[number]]: number };
-  const columnHeaderMapAnalytics: ColumnHeaderForTeamsAnalytics = columnArrayAnalytics.reduce(
-    (acc, value, index) => ({ ...acc, [value]: index + 1 }),
-    {},
-  );
-
-  const workbookAnalytics = new Excel.Workbook();
-  await workbookAnalytics.csv.readFile(`${dir}/analytics.csv`);
-  const worksheetAnalytics = workbookAnalytics.worksheets[0];
-  const dataAnalytics = [];
-
-  worksheetAnalytics.eachRow((row, rowNumber) => {
-    const rowObject = Object.entries(columnHeaderMapAnalytics).reduce((acc, value) => {
-      return {
-        ...acc,
-        [value[0]]: row.values[value[1]],
-      };
-    }, {});
-    if (rowNumber !== 1) {
-      dataAnalytics.push(rowObject);
-    }
-  });
-
-  // Money-Puck Data
-  const columnArrayMoneyPuck =
-    'team season name team position situation games_played xGoalsPercentage corsiPercentage fenwickPercentage iceTime xOnGoalFor xGoalsFor xReboundsFor xFreezeFor xPlayStoppedFor xPlayContinuedInZoneFor xPlayContinuedOutsideZoneFor flurryAdjustedxGoalsFor scoreVenueAdjustedxGoalsFor flurryScoreVenueAdjustedxGoalsFor shotsOnGoalFor missedShotsFor blockedShotAttemptsFor shotAttemptsFor goalsFor reboundsFor reboundGoalsFor freezeFor playStoppedFor playContinuedInZoneFor playContinuedOutsideZoneFor savedShotsOnGoalFor savedUnblockedShotAttemptsFor penaltiesFor penaltyMinutesFor faceOffsWonFor hitsFor takeawaysFor giveawaysFor lowDangerShotsFor mediumDangerShotsFor highDangerShotsFor lowDangerxGoalsFor mediumDangerxGoalsFor highDangerxGoalsFor lowDangerGoalsFor mediumDangerGoalsFor highDangerGoalsFor scoreAdjustedShotsAttemptsFor unblockedShotAttemptsFor scoreAdjustedUnblockedShotAttemptsFor dZoneGiveawaysFor xGoalsFromxReboundsOfShotsFor xGoalsFromActualReboundsOfShotsFor reboundxGoalsFor totalShotCreditFor scoreAdjustedTotalShotCreditFor scoreFlurryAdjustedTotalShotCreditFor xOnGoalAgainst xGoalsAgainst xReboundsAgainst xFreezeAgainst xPlayStoppedAgainst xPlayContinuedInZoneAgainst xPlayContinuedOutsideZoneAgainst flurryAdjustedxGoalsAgainst scoreVenueAdjustedxGoalsAgainst flurryScoreVenueAdjustedxGoalsAgainst shotsOnGoalAgainst missedShotsAgainst blockedShotAttemptsAgainst shotAttemptsAgainst goalsAgainst reboundsAgainst reboundGoalsAgainst freezeAgainst playStoppedAgainst playContinuedInZoneAgainst playContinuedOutsideZoneAgainst savedShotsOnGoalAgainst savedUnblockedShotAttemptsAgainst penaltiesAgainst penaltyMinutesAgainst faceOffsWonAgainst hitsAgainst takeawaysAgainst giveawaysAgainst lowDangerShotsAgainst mediumDangerShotsAgainst highDangerShotsAgainst lowDangerxGoalsAgainst mediumDangerxGoalsAgainst highDangerxGoalsAgainst lowDangerGoalsAgainst mediumDangerGoalsAgainst highDangerGoalsAgainst scoreAdjustedShotsAttemptsAgainst unblockedShotAttemptsAgainst scoreAdjustedUnblockedShotAttemptsAgainst dZoneGiveawaysAgainst xGoalsFromxReboundsOfShotsAgainst xGoalsFromActualReboundsOfShotsAgainst reboundxGoalsAgainst totalShotCreditAgainst scoreAdjustedTotalShotCreditAgainst scoreFlurryAdjustedTotalShotCreditAgainst'.split(
-      /\s+/,
-    );
-
-  type ColumnHeaderForTeamsMoneyPuck = { [C in typeof columnArrayMoneyPuck[number]]: number };
-  const columnHeaderMapMoneyPuck: ColumnHeaderForTeamsMoneyPuck = columnArrayMoneyPuck.reduce(
-    (acc, value, index) => ({ ...acc, [value]: index + 1 }),
-    {},
-  );
-
-  const workbookMoneyPuck = new Excel.Workbook();
-  await workbookMoneyPuck.csv.readFile(`${dir}/moneypuck.csv`);
-  const worksheetMoneyPuck = workbookMoneyPuck.worksheets[0];
-  const dataMoneyPuck = [];
-
-  worksheetMoneyPuck.eachRow((row, rowNumber) => {
-    const rowObject = Object.entries(columnHeaderMapMoneyPuck).reduce((acc, value) => {
-      return {
-        ...acc,
-        [value[0]]: row.values[value[1]],
-      };
-    }, {});
-
-    rowObject.offensiveAwareness =
-      parseFloat(
-        rowObject.fenwickPercentage * 0.0666 +
-          rowObject.xPlayContinuedInZoneFor * 0.0666 +
-          rowObject.flurryAdjustedxGoalsFor * 0.0666 +
-          rowObject.playContinuedInZoneFor * 0.0666 +
-          rowObject.xGoalsFromActualReboundsOfShotsFor * 0.0666 +
-          rowObject.reboundxGoalsFor * 0.0666 +
-          rowObject.scoreAdjustedShotsAttemptsFor * 0.0666 +
-          rowObject.unblockedShotAttemptsFor * 0.0666 +
-          rowObject.scoreAdjustedUnblockedShotAttemptsFor * 0.0666 +
-          rowObject.totalShotCreditFor * 0.0666 +
-          rowObject.scoreAdjustedTotalShotCreditFor * 0.0666 +
-          rowObject.scoreFlurryAdjustedTotalShotCreditFor * 0.0666 +
-          // Expected Goals
-          (rowObject.lowDangerxGoalsFor * 0.133 +
-            rowObject.mediumDangerxGoalsFor * 0.333 +
-            rowObject.highDangerxGoalsFor * 0.533) *
-            0.0666 +
-          // Rebounds
-          (rowObject.reboundGoalsFor / rowObject.reboundsFor) * 0.0666 +
-          // Goals By Zone
-          (rowObject.lowDangerGoalsFor * 0.133 +
-            rowObject.mediumDangerGoalsFor * 0.333 +
-            rowObject.highDangerGoalsFor * 0.533) *
-            0.0666 +
-          // Shots By Zone - Shots Blocked / Missed
-          (rowObject.lowDangerShotsFor * 0.133 +
-            rowObject.mediumDangerShotsFor * 0.333 +
-            rowObject.highDangerShotsFor * 0.533) *
-            0.0666 -
-          (rowObject.missedShotsFor * 0.0333 + rowObject.blockedShotAttemptsFor * 0.0333),
-      ) / rowObject.games_played;
-
-    if (rowNumber !== 1) {
-      dataMoneyPuck.push(rowObject);
-    }
-  });
-
-  // Configuring the average offensive awareness based off the model above
-  const dataMoneyPuckAll = dataMoneyPuck.filter((team) => team.situation === 'all');
-  const averageOffensiveAwareness = dataMoneyPuckAll.reduce(
-    (first, second) =>
-      parseInt(first.offensiveAwareness) > parseInt(second.offensiveAwareness)
-        ? parseFloat(first.offensiveAwareness)
-        : parseFloat(second.offensiveAwareness),
-    0,
-  );
-
-  // Trying to formulate a score
-  dataMoneyPuckAll.map((team) => {
-    team.averageOffensiveAwareness = parseFloat(team.offensiveAwareness) / averageOffensiveAwareness;
-  });
-
-  // Joining the two hockey-reference tables (right by, joining by index position)
-  const data = [];
-  for (let i = 0; i < 32; i++) {
-    const analyticElement = dataAnalytics[i];
-    const statElement = dataStats[i];
-    const element = { ...analyticElement, ...statElement };
-    data.push(element);
+      values: [req.body.content],
+    });
+    res.json(result);
+    console.log('getting data');
   }
-
-  // Response
-  res.status(200).json({
-    data: data,
-    dataMoneyPuck: dataMoneyPuck,
-  });
 }

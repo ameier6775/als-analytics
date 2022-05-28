101fa4abac21d0df776eaa1d20aba6c35889ee92
Alex Meier <ameier6775@gmail.com>
2022-05-28 16:13:34 -0500

    Configuring MySQL

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
 module.exports = nextConfig;
+
+module.exports = {
+  env: {
+    MYSQL_HOST: '127.0.0.1',
+    MYSQL_PORT: '3306',
+    MYSQL_DATABASE: 'NHL_2021',
+    MYSQL_USER: 'root',
+    MYSQL_PASSWORD: 'LetsGoBlues2019',
+  },
+};

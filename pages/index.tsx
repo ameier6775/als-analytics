a7bb89ea186d6d9835409d87022aa06b2bdf45a8
Alex Meier <ameier6775@gmail.com>
2022-05-28 16:20:18 -0500

    Switching from CSV files to MySQL db

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import axios from 'axios';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
+import { resolve } from 'path';
 import { useReducer, useState } from 'react';
 
 const Home: NextPage = () => {
@@ -10,9 +12,26 @@ const Home: NextPage = () => {
     { id: 2, name: 'National Football League', abbreviation: 'nfl' },
     { id: 3, name: 'National Hockey League', abbreviation: 'nhl' },
   ];
+
+  var playerArr = [];
+
+  // function onSubmit() {
+  //   axios
+  //     .post('./api/nhl/teams', { name: 'Jordan Kyrou' })
+  //     .then((response) => {
+  //       playerArr.push(response);
+  //       console.log(response);
+  //     })
+  //     .catch((e) => {
+  //       console.log('e: ' + e);
+  //     });
+  // }
+
   return (
     <>
       <h1 className="font-mono homeHeader">Browse Leagues</h1>
+      {/* <button onClick={(e) => onSubmit()}> Send post</button> */}
+
       {leagues.map((league) => (
         <div key={league.id} className="leagues">
           <div className="leaguesHeader">

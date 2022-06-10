018c4df65264ceae1e1407b2dc247083984875e3
Alex Meier <ameier6775@gmail.com>
2022-06-09 23:02:46 -0500

    Adding fields to the component

--- components/mlb/CompareRPitchers.js
+++ components/mlb/CompareRPitchers.js
@@ -85,10 +85,86 @@ const CompareRelieversCard = ({ comparisonRelievers }) => {
           <div className="compareContainer" key={reliever.value}>
             <h1>{reliever.value}</h1>
             <img className="teamLogo" height="10" src={teamLogos.find((obj) => obj.team === reliever.Team).logo} />
-            {/* <p>Hits For:</p>
-            <span style={{ backgroundColor: getColor(team.value, 'hAVG_rate') }}>
-              {getRatePercentage(team.hAVG_rate) + ' (' + getRank(team.hAVG_rank) + ')'}
-            </span> */}
+            <p>IP:</p>
+            <span style={{ backgroundColor: getColor(reliever.value, 'IP_rate') }}>
+              {getRatePercentage(reliever.IP_rate) + ' (' + getRank(reliever.IP_rank) + ')'}
+            </span>
+            <p>SV:</p>
+            <span style={{ backgroundColor: getColor(reliever.value, 'SV_rate') }}>
+              {getRatePercentage(reliever.SV_rate) + ' (' + getRank(reliever.SV_rank) + ')'}
+            </span>
+            <p>HLD:</p>
+            <span style={{ backgroundColor: getColor(reliever.value, 'HLD_rate') }}>
+              {getRatePercentage(reliever.HLD_rate) + ' (' + getRank(reliever.HLD_rank) + ')'}
+            </span>
+            <p>BS:</p>
+            <span style={{ backgroundColor: getColor(reliever.value, 'BS_rate') }}>
+              {getRatePercentage(reliever.BS_rate) + ' (' + getRank(reliever.BS_rank) + ')'}
+            </span>
+            <p>W:</p>
+            <span style={{ backgroundColor: getColor(reliever.value, 'W_rate') }}>
+              {getRatePercentage(reliever.W_rate) + ' (' + getRank(reliever.W_rank) + ')'}
+            </span>
+            <p>L:</p>
+            <span style={{ backgroundColor: getColor(reliever.value, 'L_rate') }}>
+              {getRatePercentage(reliever.L_rate) + ' (' + getRank(reliever.L_rank) + ')'}
+            </span>
+            <p>AVG:</p>
+            <span style={{ backgroundColor: getColor(reliever.value, 'AVG_rate') }}>
+              {getRatePercentage(reliever.AVG_rate) + ' (' + getRank(reliever.AVG_rank) + ')'}
+            </span>
+            <p>ERA:</p>
+            <span style={{ backgroundColor: getColor(reliever.value, 'ERA_rate') }}>
+              {getRatePercentage(reliever.ERA_rate) + ' (' + getRank(reliever.ERA_rank) + ')'}
+            </span>
+            <p>ERA ADJ:</p>
+            <span style={{ backgroundColor: getColor(reliever.value, 'ERA-_rate') }}>
+              {getRatePercentage(reliever['ERA-_rate']) + ' (' + getRank(reliever['ERA-_rank']) + ')'}
+            </span>
+            <p>SIERA:</p>
+            <span style={{ backgroundColor: getColor(reliever.value, 'SIERA_rate') }}>
+              {getRatePercentage(reliever.SIERA_rate) + ' (' + getRank(reliever.SIERA_rank) + ')'}
+            </span>
+            <p>WHIP:</p>
+            <span style={{ backgroundColor: getColor(reliever.value, 'WHIP_rate') }}>
+              {getRatePercentage(reliever.WHIP_rate) + ' (' + getRank(reliever.WHIP_rank) + ')'}
+            </span>
+            <p>FIP:</p>
+            <span style={{ backgroundColor: getColor(reliever.value, 'FIP_rate') }}>
+              {getRatePercentage(reliever.FIP_rate) + ' (' + getRank(reliever.FIP_rank) + ')'}
+            </span>
+            <p>FIP ADJ:</p>
+            <span style={{ backgroundColor: getColor(reliever.value, 'FIP-_rate') }}>
+              {getRatePercentage(reliever['FIP-_rate']) + ' (' + getRank(reliever['FIP-_rank']) + ')'}
+            </span>
+            <p>ERA - FIP:</p>
+            <span style={{ backgroundColor: getColor(reliever.value, 'E-F_rate') }}>
+              {getRatePercentage(reliever['E-F_rate']) + ' (' + getRank(reliever['E-F_rank']) + ')'}
+            </span>
+            <p>K / 9:</p>
+            <span style={{ backgroundColor: getColor(reliever.value, 'K/9_rate') }}>
+              {getRatePercentage(reliever['K/9_rate']) + ' (' + getRank(reliever['K/9_rank']) + ')'}
+            </span>
+            <p>BB / 9:</p>
+            <span style={{ backgroundColor: getColor(reliever.value, 'BB/9_rate') }}>
+              {getRatePercentage(reliever['BB/9_rate']) + ' (' + getRank(reliever['BB/9_rank']) + ')'}
+            </span>
+            <p>HR / 9:</p>
+            <span style={{ backgroundColor: getColor(reliever.value, 'HR/9_rate') }}>
+              {getRatePercentage(reliever['HR/9_rate']) + ' (' + getRank(reliever['HR/9_rank']) + ')'}
+            </span>
+            <p>LOB %:</p>
+            <span style={{ backgroundColor: getColor(reliever.value, 'LOB%_rate') }}>
+              {getRatePercentage(reliever['LOB%_rate']) + ' (' + getRank(reliever['LOB%_rank']) + ')'}
+            </span>
+            <p>TBF:</p>
+            <span style={{ backgroundColor: getColor(reliever.value, 'TBF_rate') }}>
+              {getRatePercentage(reliever.TBF_rate) + ' (' + getRank(reliever.TBF_rank) + ')'}
+            </span>
+            <p>WP:</p>
+            <span style={{ backgroundColor: getColor(reliever.value, 'WP_rate') }}>
+              {getRatePercentage(reliever.WP_rate) + ' (' + getRank(reliever.WP_rank) + ')'}
+            </span>
           </div>
         ))
       ) : (

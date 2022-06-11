9e1cd729134d34e352a5da9f01e4266f25828f2f
Alex Meier <ameier6775@gmail.com>
2022-06-11 16:34:57 -0500

    Adding numbers to each field

--- components/mlb/CompareSPitchers.js
+++ components/mlb/CompareSPitchers.js
@@ -85,77 +85,91 @@ const CompareStartersCard = ({ comparisonStarters }) => {
           <div className="compareContainer" key={starter.value}>
             <h1>{starter.value}</h1>
             <img className="teamLogo" height="10" src={teamLogos.find((obj) => obj.team === starter.Team).logo} />
-            <p>GS:</p>
-            <span style={{ backgroundColor: getColor(starter.value, 'GS_rate') }}>
-              {getRatePercentage(starter.GS_rate) + ' (' + getRank(starter.GS_rank) + ')'}
-            </span>
-            <p>IP:</p>
-            <span style={{ backgroundColor: getColor(starter.value, 'IP_rate') }}>
-              {getRatePercentage(starter.IP_rate) + ' (' + getRank(starter.IP_rank) + ')'}
-            </span>
+            <h3 className="fieldSubheader">Record:</h3>
             <p>W:</p>
             <span style={{ backgroundColor: getColor(starter.value, 'W_rate') }}>
-              {getRatePercentage(starter.W_rate) + ' (' + getRank(starter.W_rank) + ')'}
+              {starter.W + ' - ' + getRatePercentage(starter.W_rate) + ' (' + getRank(starter.W_rank) + ')'}
             </span>
             <p>L:</p>
             <span style={{ backgroundColor: getColor(starter.value, 'L_rate') }}>
-              {getRatePercentage(starter.L_rate) + ' (' + getRank(starter.L_rank) + ')'}
+              {starter.L + ' - ' + getRatePercentage(starter.L_rate) + ' (' + getRank(starter.L_rank) + ')'}
             </span>
+            <h3 className="fieldSubheader">Usage:</h3>
+            <p>GS:</p>
+            <span style={{ backgroundColor: getColor(starter.value, 'GS_rate') }}>
+              {starter.GS + ' - ' + getRatePercentage(starter.GS_rate) + ' (' + getRank(starter.GS_rank) + ')'}
+            </span>
+            <p>IP:</p>
+            <span style={{ backgroundColor: getColor(starter.value, 'IP_rate') }}>
+              {starter.IP + ' - ' + getRatePercentage(starter.IP_rate) + ' (' + getRank(starter.IP_rank) + ')'}
+            </span>
+            <h3 className="fieldSubheader">Production:</h3>
             <p>AVG:</p>
             <span style={{ backgroundColor: getColor(starter.value, 'AVG_rate') }}>
-              {getRatePercentage(starter.AVG_rate) + ' (' + getRank(starter.AVG_rank) + ')'}
+              {starter.AVG + ' - ' + getRatePercentage(starter.AVG_rate) + ' (' + getRank(starter.AVG_rank) + ')'}
             </span>
             <p>ERA:</p>
             <span style={{ backgroundColor: getColor(starter.value, 'ERA_rate') }}>
-              {getRatePercentage(starter.ERA_rate) + ' (' + getRank(starter.ERA_rank) + ')'}
-            </span>
-            <p>ERA ADJ:</p>
-            <span style={{ backgroundColor: getColor(starter.value, 'ERA-_rate') }}>
-              {getRatePercentage(starter['ERA-_rate']) + ' (' + getRank(starter['ERA-_rank']) + ')'}
-            </span>
-            <p>SIERA:</p>
-            <span style={{ backgroundColor: getColor(starter.value, 'SIERA_rate') }}>
-              {getRatePercentage(starter.SIERA_rate) + ' (' + getRank(starter.SIERA_rank) + ')'}
-            </span>
-            <p>WHIP:</p>
-            <span style={{ backgroundColor: getColor(starter.value, 'WHIP_rate') }}>
-              {getRatePercentage(starter.WHIP_rate) + ' (' + getRank(starter.WHIP_rank) + ')'}
+              {starter.ERA + ' - ' + getRatePercentage(starter.ERA_rate) + ' (' + getRank(starter.ERA_rank) + ')'}
             </span>
             <p>FIP:</p>
             <span style={{ backgroundColor: getColor(starter.value, 'FIP_rate') }}>
-              {getRatePercentage(starter.FIP_rate) + ' (' + getRank(starter.FIP_rank) + ')'}
+              {starter.FIP + ' - ' + getRatePercentage(starter.FIP_rate) + ' (' + getRank(starter.FIP_rank) + ')'}
+            </span>
+            <p>WHIP:</p>
+            <span style={{ backgroundColor: getColor(starter.value, 'WHIP_rate') }}>
+              {starter.WHIP + ' - ' + getRatePercentage(starter.WHIP_rate) + ' (' + getRank(starter.WHIP_rank) + ')'}
+            </span>
+            <h3 className="fieldSubheader">Projection:</h3>
+            <p>ERA ADJ:</p>
+            <span style={{ backgroundColor: getColor(starter.value, 'ERA-_rate') }}>
+              {starter['ERA-'] +
+                ' - ' +
+                getRatePercentage(starter['ERA-_rate']) +
+                ' (' +
+                getRank(starter['ERA-_rank']) +
+                ')'}
             </span>
             <p>FIP ADJ:</p>
             <span style={{ backgroundColor: getColor(starter.value, 'FIP-_rate') }}>
-              {getRatePercentage(starter['FIP-_rate']) + ' (' + getRank(starter['FIP-_rank']) + ')'}
+              {starter['FIP-'] +
+                ' - ' +
+                getRatePercentage(starter['FIP-_rate']) +
+                ' (' +
+                getRank(starter['FIP-_rank']) +
+                ')'}
             </span>
-            <p>ERA - FIP:</p>
-            <span style={{ backgroundColor: getColor(starter.value, 'E-F_rate') }}>
-              {getRatePercentage(starter['E-F_rate']) + ' (' + getRank(starter['E-F_rank']) + ')'}
+            <p>SIERA:</p>
+            <span style={{ backgroundColor: getColor(starter.value, 'SIERA_rate') }}>
+              {starter.SIERA + ' - ' + getRatePercentage(starter.SIERA_rate) + ' (' + getRank(starter.SIERA_rank) + ')'}
             </span>
+            <h3 className="fieldSubheader">Per 9:</h3>
             <p>K / 9:</p>
             <span style={{ backgroundColor: getColor(starter.value, 'K/9_rate') }}>
-              {getRatePercentage(starter['K/9_rate']) + ' (' + getRank(starter['K/9_rank']) + ')'}
+              {starter['K/9'] +
+                ' - ' +
+                getRatePercentage(starter['K/9_rate']) +
+                ' (' +
+                getRank(starter['K/9_rank']) +
+                ')'}
             </span>
             <p>BB / 9:</p>
             <span style={{ backgroundColor: getColor(starter.value, 'BB/9_rate') }}>
-              {getRatePercentage(starter['BB/9_rate']) + ' (' + getRank(starter['BB/9_rank']) + ')'}
+              {starter['BB/9'] +
+                ' - ' +
+                getRatePercentage(starter['BB/9_rate']) +
+                ' (' +
+                getRank(starter['BB/9_rank']) +
+                ')'}
             </span>
             <p>HR / 9:</p>
             <span style={{ backgroundColor: getColor(starter.value, 'HR/9_rate') }}>
-              {getRatePercentage(starter['HR/9_rate']) + ' (' + getRank(starter['HR/9_rank']) + ')'}
-            </span>
-            <p>LOB %:</p>
-            <span style={{ backgroundColor: getColor(starter.value, 'LOB%_rate') }}>
-              {getRatePercentage(starter['LOB%_rate']) + ' (' + getRank(starter['LOB%_rank']) + ')'}
-            </span>
-            <p>TBF:</p>
-            <span style={{ backgroundColor: getColor(starter.value, 'TBF_rate') }}>
-              {getRatePercentage(starter.TBF_rate) + ' (' + getRank(starter.TBF_rank) + ')'}
-            </span>
-            <p>WP:</p>
-            <span style={{ backgroundColor: getColor(starter.value, 'WP_rate') }}>
-              {getRatePercentage(starter.WP_rate) + ' (' + getRank(starter.WP_rank) + ')'}
+              {starter['HR/9'] +
+                ' - ' +
+                getRatePercentage(starter['HR/9_rate']) +
+                ' (' +
+                getRank(starter['HR/9_rank']) +
+                ')'}
             </span>
           </div>
         ))
